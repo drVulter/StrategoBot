31a8870777f195e886e7d4902ed4b627dd5f9f12
Quinn Stratton <qgstratton@gmail.com>
2018-02-15 14:52:00 -0600

    Added initial directories

---
+++ ExistingEngine/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stratego
+{
+    public class Player
+    {
+        private string _name;
+        private SpaceType _playerType;
+        private PlayerColor _playerColor;
+
+        public string name
+        {
+            get { return _name; }
+        }
+        public SpaceType playerType
+        {
+            get { return _playerType; }
+        }
+        public PlayerColor playerColor
+        {
+            get { return _playerColor; }
+        }
+        public Player(string name,SpaceType playerType,PlayerColor playerColor)
+        {
+            _name = name;
+            _playerType = playerType;
+            _playerColor = playerColor;
+        }
+    }
+
+    public enum PlayerColor { Red=1,Blue=2}
+}
---
+++ ExistingEngine/engine-readme.md
@@ -0,0 +1,3 @@
+# This contains the original game engine that StrategoBot is built on 
+This code was written by JuustoMestari on github
+[Original Repository](https://github.com/JuustoMestari/Stratego)
