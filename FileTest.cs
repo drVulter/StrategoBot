9dc1def4216ee9493c60633b8ffc937f07238c08
Quinn Stratton <qgstratton@gmail.com>
2018-04-07 13:57:07 -0500

    added data directory with results

---
+++ FileTest.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class FileTest
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        Console.WriteLine(rnd.Next(10));
+    }
+}
